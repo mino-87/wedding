fa70c7b2f7d1664bbed313388ba73a3e319c39be
mino-87 <minafikry87@gmail.com>
2026-09-11 14:11:16 +0200

    chore: refresh runtime cache key

--- enhancements.js
+++ enhancements.js
@@ -1,1 +1,1 @@
-const weddingRuntime=document.createElement('script');weddingRuntime.src='runtime-fixes.js?v=20260911e';document.head.appendChild(weddingRuntime);
+const weddingRuntime=document.createElement('script');weddingRuntime.src='runtime-fixes.js?v=20260911f';document.head.appendChild(weddingRuntime);
