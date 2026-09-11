4c87ba07b356e095280d799ea8a2b2effd4c3136
mino-87 <minafikry87@gmail.com>
2026-09-11 06:46:06 +0200

    Add wedding configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+window.WEDDING_CONFIG = Object.freeze({
+  eventIso: "2026-09-24T17:00:00+03:00",
+  maxUploadBytes: 250 * 1024 * 1024,
+  maxFilesPerBatch: 20,
+  uploadEndpoint: "",
+  musicUrl: "",
+  faceLandmarkerModelUrl: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
+  faceTasksVisionBaseUrl: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22-rc.20250304/wasm",
+  faceTasksVisionModuleUrl: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22-rc.20250304/+esm"
+});
